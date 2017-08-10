22310787d9db0d5dbfa9ad1f61045c32f81938e2
Hubert Bugaj <lesny.rumcajs@gmail.com>
2017-08-10 20:32:00 +0200

    clang-format llvm

--- challenge1/challenge1_test.cpp
+++ challenge1/challenge1_test.cpp
@@ -1,10 +1,13 @@
 // Copyright 2017, Hubert Bugaj
 // Challenge 1 Test
-#include <string>
 #include <gtest/gtest.h>
+#include <string>
 
 #include "./challenge1.h"
 
-TEST (Challenge1Test, BasicTest) { 
-    EXPECT_EQ ("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", challenge1::hex2base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
+TEST(Challenge1Test, BasicTest) {
+  EXPECT_EQ("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+            challenge1::hex2base64("49276d206b696c6c696e6720796f757220627261696"
+                                   "e206c696b65206120706f69736f6e6f7573206d7573"
+                                   "68726f6f6d"));
 }
--- challenge2/challenge2.h
+++ challenge2/challenge2.h
@@ -6,19 +6,19 @@
 #include <botan/base64.h>
 #include <botan/hex.h>
 
-#include <string>
 #include <functional>
+#include <string>
 
 namespace challenge2 {
-    std::string fixed_xor(const std::string& input_hex, const std::string& key_hex) {
-        auto data_binary = Botan::hex_decode(input_hex);
-        auto key_binary = Botan::hex_decode(key_hex);
+std::string fixed_xor(const std::string &input_hex,
+                      const std::string &key_hex) {
+  auto data_binary = Botan::hex_decode(input_hex);
+  auto key_binary = Botan::hex_decode(key_hex);
 
-        std::transform(data_binary.begin(), data_binary.end(),
-                                        key_binary.begin(), data_binary.begin(),
-                                        [](auto e1, auto e2) {return e1^e2;});
+  std::transform(data_binary.begin(), data_binary.end(), key_binary.begin(),
+                 data_binary.begin(), [](auto e1, auto e2) { return e1 ^ e2; });
 
-        return Botan::hex_encode(data_binary, false);
-    }
-}  // namespace challenge2
-#endif  //  CHALLENGE_2_CHALLENGE2_H_
+  return Botan::hex_encode(data_binary, false);
+}
+} // namespace challenge2
+#endif //  CHALLENGE_2_CHALLENGE2_H_
