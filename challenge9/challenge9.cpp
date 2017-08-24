71453f0cf792a765403950c74c4af8b3cd402d6d
Hubert Bugaj <lesny.rumcajs@gmail.com>
2017-08-24 20:42:20 +0200

    added more tests to challenge9 (pkcs7 padding)

--- challenge9/challenge9.cpp
+++ challenge9/challenge9.cpp
@@ -1,7 +1,13 @@
 #include "./challenge9.h"
 
+#include <stdexcept>
+
 void challenge9::addPKCS7Padding(std::vector<uint8_t> &data,
                                  std::size_t block_length) {
+  if (block_length < 1) {
+    throw std::out_of_range("Invalid block size");
+  }
+  
   auto missing_bytes = block_length - data.size() % block_length;
   if (0 == missing_bytes) {
     missing_bytes = block_length;
--- challenge9/challenge9_test.cpp
+++ challenge9/challenge9_test.cpp
@@ -6,13 +6,36 @@
 #include "./challenge9.h"
 
 TEST(Challenge9Test, BasicTest) {
-  std::string plaintext = "YELLOW SUBMARINE";
-  std::vector<uint8_t> result(plaintext.begin(), plaintext.end());
 
   std::vector<uint8_t> expected{'Y', 'E', 'L',  'L',  'O',  'W', ' ',
                                 'S', 'U', 'B',  'M',  'A',  'R', 'I',
                                 'N', 'E', 0x04, 0x04, 0x04, 0x04};
+
+  std::string plaintext = "YELLOW SUBMARINE";
+  std::vector<uint8_t> result(plaintext.begin(), plaintext.end());
   challenge9::addPKCS7Padding(result, 20);
 
   EXPECT_EQ(result, expected);
+}
+
+TEST(Challenge9Test, PlaintextMultipleOfBlock) {
+  std::vector<uint8_t> expected{'Y', 'E', 'L',  0x03,  0x03,  0x03};
+  std::vector<uint8_t> result{'Y', 'E', 'L'};
+
+  challenge9::addPKCS7Padding(result, 3);
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(Challenge9Test, EmptyPlaintext) {
+  std::vector<uint8_t> expected(20, 0x14);
+  std::vector<uint8_t> result;
+  challenge9::addPKCS7Padding(result, 20);
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(Challenge9Test, NonPositiveBlockSizeShouldThrowOutOfRange) {
+  std::vector<uint8_t> data;
+  EXPECT_THROW(challenge9::addPKCS7Padding(data, 0), std::out_of_range);
 }
